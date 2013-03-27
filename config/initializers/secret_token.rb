0d2e91b1fab256bb322eb7556e5795e494b94ea3
Samuel Kobe <samuelkobe@gmail.com>
2013-03-27 13:21:23 -0700

    inital commit with jquery min and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evy::Application.config.secret_token = '418d743a323a463ff75f897a8301ff9f8528b8a7e3a584eed07d67b8900070269b3ad555bb2f3fe9ee0b16cbde47360e43b32a3567dbba9a580e72ff4f3feb1b'

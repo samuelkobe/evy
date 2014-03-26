256bc16881d796fe3af23d9d9fa26d151260f21f
Samuel Kobe <samuelkobe@gmail.com>
2014-03-26 13:59:13 -0700

    css uptodate banners added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_filter :recent_work
+  before_filter :services
   before_filter :featured_work
   
   def recent_work
     @recent_work = Refinery::Works::Work.order('position DESC').first(4)
+  end
+  
+  def services
+    @services = Refinery::Services::Service.order('position ASC').each
   end
   
   def featured_work
